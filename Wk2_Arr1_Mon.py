604a0b8ddea056bd9f14f56e88e132296c9b482a
Steve Duncan <steve.duncan.email@gmail.com>
2016-06-14 13:12:42 -0700

    add function InsertAt

--- Wk2_Arr1_Mon.py
+++ Wk2_Arr1_Mon.py
@@ -14,9 +14,40 @@ def PushFront(arr,valToInsert):
 	return arr
 
 
+def InsertAt(arr,ndx,val):
+	#initialize string to hold comma separated values from array
+ 	valToInsert = ""
+ 	#iterate the array up to the passed index
+ 	for i in range (0,ndx):
+ 		#concatenate elements from array as comma separated strings
+ 	 	if i == 0:
+ 	 		valToInsert += str(arr[i])
+ 	 	else:
+	 		valToInsert += ("," + str(arr[i]))
+	#concatenate value to add to string
+	valToInsert += ("," + str(val))
+	#iterate remaining elements from array after index
+	for i in range (ndx,len(arr)):
+		#concatenate remaining values to variable
+		valToInsert += ("," + str(arr[i]))
+	#split the comma seperatev variable, convert to integer and reload array
+	arr = [int(e) for e in valToInsert.split(',')]
+	return arr
+
+
 print "\n############# Push Front #################"
 arr = [34, 57, 89, 35,21]	#starting array
 valToInsert = 53			#value to insert
 print "Before insert:",arr
 print "Value to insert:",valToInsert
 print "After insert:",PushFront(arr,valToInsert)
+
+
+print "\n############## Insert At ##################"
+arr = [34, 57, 89, 35,21]	#starting array
+ndx = 4						#index to add val
+valToInsert = 66					#value to insert
+print "Before insert:",arr
+print "Value to insert:",valToInsert
+print "Insert at index:",ndx
+print "After insert:",InsertAt(arr,ndx,valToInsert)
